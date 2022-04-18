fc7ecdf2db5e64d7bfeab3e6d655468f33c5d484
Ivan Andres Diaz <ivanandresdilox@gmail.com>
2022-04-18 09:12:54 -0500

    se agrega react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import './App.css';
+import "./App.css";
 import Home from "./components/Home/Home";
 import Oficinas from "./components/Oficinas/Oficinas";
 import Formulario from "./components/Formulario/Formulario";
-import {createTheme} from "@mui/material/styles";
-import {ThemeProvider} from "@mui/material";
-
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const theme = createTheme({
-  palette:{
-    primary:{
-      main: '#3F3F41'
-    }
-  }
-})
+  palette: {
+    primary: {
+      main: "#3F3F41",
+    },
+  },
+});
 
 function App() {
   return (
-      <ThemeProvider theme={theme}>
-          {/*<Home/>*/}
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="////formulario" element={<Formulario />} />
           {/*<Oficinas/>*/}
-          <Formulario/>
-      </ThemeProvider>
+          {/* <Formulario/> */}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
